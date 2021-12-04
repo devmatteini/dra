e8951e93ae9d04b03e8469c97e5fb5535211e240
devmatteini <dev.matteini@gmail.com>
2021-12-04 18:21:16 +0100

    cli: implemented DownloadSpinner

---
+++ src/cli/download_spinner.rs
@@ -0,0 +1,36 @@
+use indicatif::{ProgressBar, ProgressStyle};
+
+// NOTE: spinner ticks and duration are from
+// https://github.com/sindresorhus/cli-spinners/blob/00de8fbeee16fa49502fa4f687449f70f2c8ca2c/spinners.json#L2-L16
+const TICKS: &[&str] = &["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+const TICK_DURATION: u64 = 80;
+
+pub struct DownloadSpinner {
+    pb: ProgressBar,
+    asset_name: String,
+}
+
+impl DownloadSpinner {
+    pub fn new(asset_name: &str) -> Self {
+        let pb = ProgressBar::new_spinner();
+        pb.set_style(
+            ProgressStyle::default_spinner()
+                .tick_strings(TICKS)
+                .template("{spinner:.blue} {msg}"),
+        );
+        pb.set_message(format!("Downloading {}", asset_name));
+        Self {
+            pb,
+            asset_name: asset_name.to_string(),
+        }
+    }
+
+    pub fn start(&self) {
+        self.pb.enable_steady_tick(TICK_DURATION);
+    }
+
+    pub fn finish(&self) {
+        self.pb.finish_and_clear();
+        println!("Download {} completed!", &self.asset_name);
+    }
+}
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,2 +1,3 @@
+pub mod download_spinner;
 mod parse_repository;
 pub mod root_command;
