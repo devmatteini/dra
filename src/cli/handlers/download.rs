8587438f7b8101d820be3227eb837ae76635c84a
devmatteini <dev.matteini@gmail.com>
2021-12-05 17:55:40 +0100

    cli: refactor DownloadHandler to improve readability

--- src/cli/handlers/download.rs
+++ src/cli/handlers/download.rs
@@ -1,6 +1,7 @@
 use crate::cli::download_spinner::DownloadSpinner;
 use crate::cli::handlers::HandlerResult;
 use crate::github;
+use crate::github::release::{Asset, Release};
 use crate::github::Repository;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Select;
@@ -16,31 +17,51 @@ impl DownloadHandler {
     }
 
     pub fn run(&self) -> HandlerResult {
-        let release = github::latest_release(&self.repository).map_err(|e| e.to_string())?;
-        let items = release
-            .assets
-            .iter()
-            .map(|x| x.name.clone())
-            .collect::<Vec<String>>();
-        let selected = Select::with_theme(&ColorfulTheme::default())
+        let release = self.fetch_latest_release()?;
+        let selected_asset = Self::ask_select_asset(release.assets);
+        Self::download_asset(&selected_asset)?;
+        Ok(())
+    }
+
+    fn fetch_latest_release(&self) -> Result<Release, String> {
+        github::latest_release(&self.repository).map_err(|e| e.to_string())
+    }
+
+    fn ask_select_asset(assets: Vec<Asset>) -> Asset {
+        let items = Self::assets_names(&assets);
+        let index = Select::with_theme(&ColorfulTheme::default())
             .with_prompt("Pick the asset to download")
             .default(0)
             .items(&items)
+            // TODO: check out interact_out() to allow user to exit selection and stop download
             .interact()
             .unwrap();
-        let selected_name = &items[selected];
-        let selected_asset = release
-            .assets
-            .into_iter()
-            .find(|x| &x.name == selected_name)
-            .unwrap();
-        let spinner = DownloadSpinner::new(selected_name);
+        let selected_name = &items[index];
+        Self::find_asset_by_name(selected_name, assets)
+    }
+
+    fn download_asset(selected_asset: &Asset) -> Result<(), String> {
+        let spinner = DownloadSpinner::new(&selected_asset.name);
         spinner.start();
         let mut stream = github::download_asset(&selected_asset).map_err(|e| e.to_string())?;
-        let mut destination = File::create(&selected_name).unwrap();
+        let mut destination = Self::create_file(&selected_asset.name)?;
         std::io::copy(&mut stream, &mut destination).unwrap();
         spinner.finish();
-
         Ok(())
+    }
+
+    fn create_file(selected_name: &String) -> Result<File, String> {
+        File::create(&selected_name).map_err(|e| e.to_string())
+    }
+
+    fn assets_names(assets: &Vec<Asset>) -> Vec<String> {
+        assets
+            .iter()
+            .map(|x| x.name.clone())
+            .collect::<Vec<String>>()
+    }
+
+    fn find_asset_by_name(name: &String, assets: Vec<Asset>) -> Asset {
+        assets.into_iter().find(|x| &x.name == name).unwrap()
     }
 }
