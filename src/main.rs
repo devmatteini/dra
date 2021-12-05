786cfc3eca3fc6c865f726a5d3f9e64eee4b1dca
devmatteini <dev.matteini@gmail.com>
2021-12-05 17:17:15 +0100

    implemented main() function with args parsing and command dispatching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use crate::cli::handlers::download::DownloadHandler;
+use crate::cli::root_command::{Cli, Command};
+use std::process::exit;
+use structopt::StructOpt;
+
+mod cli;
+mod github;
+
 fn main() {
-    println!("Hello, world!");
+    let cli: Cli = Cli::from_args();
+    match cli.cmd {
+        Command::Download => {
+            if let Err(e) = DownloadHandler::new(cli.repo).run() {
+                eprintln!("{}", e);
+                exit(1)
+            }
+        }
+    }
 }
