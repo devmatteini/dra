30d80c9b1acf1354d7d65e7ab744e56d8a31a8b8
devmatteini <dev.matteini@gmail.com>
2021-12-08 17:28:18 +0100

    cli - handlers: implemented auto select of asset to download

--- src/cli/handlers/download.rs
+++ src/cli/handlers/download.rs
@@ -3,6 +3,7 @@ use crate::cli::handlers::select;
 use crate::cli::handlers::{HandlerError, HandlerResult};
 use crate::github;
 use crate::github::release::{Asset, Release};
+use crate::github::tagged_asset::TaggedAsset;
 use crate::github::{DownloadAssetError, ReleaseError, Repository};
 use std::fs::File;
 
@@ -18,9 +19,29 @@ impl DownloadHandler {
 
     pub fn run(&self) -> HandlerResult {
         let release = self.fetch_latest_release()?;
-        let selected_asset = Self::ask_select_asset(release.assets)?;
+
+        let selected_asset = if let Some(untagged) = self.select.as_ref() {
+            Self::autoselect_asset(release, untagged)
+        } else {
+            Self::ask_select_asset(release.assets)
+        }?;
+
         Self::download_asset(&selected_asset)?;
         Ok(())
+    }
+
+    fn autoselect_asset(release: Release, untagged: &str) -> Result<Asset, HandlerError> {
+        let asset_name = TaggedAsset::tag(&release.tag, untagged);
+        let opt_asset = release.assets.into_iter().find(|x| x.name == asset_name);
+
+        if let Some(asset) = opt_asset {
+            Ok(asset)
+        } else {
+            Err(HandlerError::new(format!(
+                "No asset found for {}",
+                untagged
+            )))
+        }
     }
 
     fn fetch_latest_release(&self) -> Result<Release, HandlerError> {
