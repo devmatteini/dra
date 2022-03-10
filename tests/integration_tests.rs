5f173c89b0270989e40709181f268aef9104c162
Cosimo Matteini <dev.matteini@gmail.com>
2022-03-10 22:54:08 +0100

    tests: add integration tests on installation from tar archives

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -33,3 +33,35 @@ mod debian {
         assert_contains("requires superuser privilege", &output);
     }
 }
+
+#[cfg(test)]
+mod tar_archive {
+    use crate::assertions::{assert_contains, assert_error, assert_success};
+    use crate::docker::{images, Docker, ExecArgs};
+
+    #[test]
+    fn installed_successfully() {
+        let container = Docker::run(images::UBUNTU);
+
+        let result = container.exec(
+            "dra devmatteini/dra-tests download -s helloworld.tar.gz -i",
+            ExecArgs::Default,
+        );
+
+        let output = assert_success(result);
+        assert_contains("Installation completed", &output);
+    }
+
+    #[test]
+    fn no_executable() {
+        let container = Docker::run(images::UBUNTU);
+
+        let result = container.exec(
+            "dra devmatteini/dra-tests download -s no_executable.tar.gz -i",
+            ExecArgs::Default,
+        );
+
+        let output = assert_error(result);
+        assert_contains("No executable found", &output);
+    }
+}
