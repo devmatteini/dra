ad274c9c591e7dc9c7dbe431609f313866742619
Cosimo Matteini <dev.matteini@gmail.com>
2022-07-09 16:43:26 +0200

    cargo: bump uuid from 0.8.2 to 1.1.2
    See migration guide: https://github.com/uuid-rs/uuid/releases/tag/1.0.0

--- src/cli/temp_file.rs
+++ src/cli/temp_file.rs
@@ -4,12 +4,12 @@ use uuid::Uuid;
 
 pub fn temp_file() -> PathBuf {
     let mut temp_dir = std::env::temp_dir();
-    temp_dir.push(format!("dra-{}", Uuid::new_v4().to_simple()));
+    temp_dir.push(format!("dra-{}", Uuid::new_v4().simple()));
     temp_dir
 }
 
 pub fn temp_dir() -> PathBuf {
     let mut temp_dir = std::env::temp_dir();
-    temp_dir.push(format!("dra-{}/", Uuid::new_v4().to_simple()));
+    temp_dir.push(format!("dra-{}/", Uuid::new_v4().simple()));
     temp_dir
 }
