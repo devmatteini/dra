330e6583b84eead8d1c1f27863fd107429148eba
Cosimo Matteini <dev.matteini@gmail.com>
2022-10-16 18:22:27 +0200

    scripts - create-release-archive: add second argument to specify operating system

--- scripts/create-release-archive.sh
+++ scripts/create-release-archive.sh
@@ -2,17 +2,33 @@
 
 set -eo pipefail
 
+LINUX="linux"
+MACOS="macos"
+WINDOWS="windows"
+
 function usage() {
-  echo "Usage: $(basename "$0") <version>"
+  echo
+  echo "Usage: $(basename "$0") <version> <os>"
   echo
   echo "ARGS:"
   echo "    <version> version using format x.y.z"
+  echo "    <os>      supported os: linux, macos, windows"
   exit 1
 }
 
-if [[ -z $1 ]]; then usage; fi
+function is_supported_os() {
+  if [[ "$1" != "$LINUX" && "$1" != "$MACOS" && "$1" != "$WINDOWS" ]]; then
+    echo "Error: '$1' is not valid os"
+    return 1
+  fi
+}
+
+if [[ -z $1 || -z $2 ]]; then usage; fi
+if ! is_supported_os "$2"; then usage; fi
 
 version="$1"
+os="$2"
+
 output="dra-$version"
 archive="${output}.tar.gz"
 
