58a832a60d1e21811dd0a2df09ae3d37f67491b5
Cosimo Matteini <dev.matteini@gmail.com>
2022-11-01 16:38:35 +0100

    tests: run windows integration tests with proper release assets for windows
    This is also resolved by releasing devmatteini/dra-tests 0.1.5

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -6,10 +6,32 @@ mod archives {
     use assert_cmd::assert::OutputAssertExt;
     use assert_cmd::prelude::CommandCargoExt;
 
+    #[cfg(target_family = "unix")]
     #[test_case("helloworld.tar.gz"; "tar gzip")]
     #[test_case("helloworld.tar.bz2"; "tar bzip2")]
     #[test_case("helloworld.tar.xz"; "tar xz")]
     #[test_case("helloworld.zip"; "zip")]
+    fn installed_successfully(asset: &str) {
+        let output_dir = path_to_string(any_temp_dir());
+
+        let mut cmd = Command::cargo_bin("dra").unwrap();
+
+        let result = cmd
+            .arg("download")
+            .args(&["-s", asset])
+            .args(&["-o", &output_dir])
+            .arg("-i")
+            .arg("devmatteini/dra-tests")
+            .assert();
+
+        result
+            .success()
+            .stdout(predicates::str::contains("Installation completed"));
+    }
+
+    #[cfg(target_os = "windows")]
+    #[test_case("helloworld-windows.tar.gz"; "tar gzip")]
+    #[test_case("helloworld-windows.zip"; "zip")]
     fn installed_successfully(asset: &str) {
         let output_dir = path_to_string(any_temp_dir());
 
