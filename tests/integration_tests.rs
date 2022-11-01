61bf3cebf3f853a818db20db406ab8cc143a0f72
Cosimo Matteini <dev.matteini@gmail.com>
2022-11-01 15:32:55 +0100

    tests: extract debian module into its own file

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,39 +1,3 @@
-mod assertions;
-mod docker;
-
-#[cfg(target_os = "linux")]
-mod debian {
-    use crate::assertions::{assert_contains, assert_error, assert_success};
-    use crate::docker::{images, users, Docker, ExecArgs};
-
-    #[test]
-    fn installed_successfully() {
-        let container = Docker::run(images::UBUNTU);
-
-        let result = container.exec(
-            "dra download -s helloworld.deb -i devmatteini/dra-tests",
-            ExecArgs::Default,
-        );
-
-        let output = assert_success(result);
-        assert_contains("Installation completed", &output);
-    }
-
-    #[test]
-    fn wrong_privileges() {
-        let container = Docker::run(images::UBUNTU);
-
-        let result = container.exec(
-            "dra download -s helloworld.deb -i devmatteini/dra-tests",
-            ExecArgs::User(users::TESTER.into()),
-        );
-
-        let output = assert_error(result);
-        assert_contains("dpkg", &output);
-        assert_contains("requires superuser privilege", &output);
-    }
-}
-
 mod archives {
     use std::path::PathBuf;
     use std::process::Command;
