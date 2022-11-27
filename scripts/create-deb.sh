ef066cd3f1b59d83d580ec202c6eb5ed37f32c5c
Cosimo Matteini <dev.matteini@gmail.com>
2022-11-27 17:02:50 +0100

    release: pass target to create-release-archive and create-deb scripts
    This patch allowed to simplify the create-release-archive.sh and remove data
    duplication between the release.yml and the script.
    
    To simplify even more, during the release.yml job the CARGO_TARGET is
    always set so that the pattern 'target/{target}/release' is consistent.
    
    Due to the above change, the create-deb.sh need to set the --target options
    as well.

--- scripts/create-deb.sh
+++ scripts/create-deb.sh
@@ -7,9 +7,16 @@ if ! command -V cargo-deb >/dev/null; then
   exit 1
 fi
 
-output="$(cargo deb --no-build --output .)"
+if [[ -z $1 ]]; then
+  echo "Missing target argument"
+  exit 1
+fi
+
+target="$1"
+
+output="$(cargo deb --target "$target" --no-build --output .)"
 
 # The output is like './dra_x.y.z_amd64.deb'.
 # We need to remove './' with bash replace so we can use it
 # as the asset name when uploading to github release
-echo "${output/.\/}"
+echo "${output/.\//}"
--- scripts/create-release-archive.sh
+++ scripts/create-release-archive.sh
@@ -9,11 +9,12 @@ WINDOWS="windows"
 
 function usage() {
   echo
-  echo "Usage: $(basename "$0") <version> <os>"
+  echo "Usage: $(basename "$0") <version> <os> <target>"
   echo
   echo "ARGS:"
-  echo "    <version> version using format x.y.z"
-  echo "    <os>      supported os: $LINUX, $MACOS, $WINDOWS, $LINUX_ARMV6"
+  echo "    <version>     version using format x.y.z"
+  echo "    <os>          supported os: $LINUX, $MACOS, $WINDOWS, $LINUX_ARMV6"
+  echo "    <target>      build target name (https://doc.rust-lang.org/rustc/platform-support.html)"
   exit 1
 }
 
@@ -21,21 +22,6 @@ function is_supported_os() {
   if [[ "$1" != "$LINUX" && "$1" != "$MACOS" && "$1" != "$WINDOWS" && "$1" != "$LINUX_ARMV6" ]]; then
     echo "Error: '$1' is not valid os"
     return 1
-  fi
-}
-
-function target_by_os() {
-  if [[ "$1" == "$LINUX" ]]; then
-    echo "x86_64-unknown-linux-gnu"
-  elif [[ "$1" == "$MACOS" ]]; then
-    echo "x86_64-apple-darwin"
-  elif [[ "$1" == "$WINDOWS" ]]; then
-    echo "x86_64-pc-windows-msvc"
-  elif [[ "$1" == "$LINUX_ARMV6" ]]; then
-    echo "arm-unknown-linux-gnueabihf"
-  else
-    echo "Error: cannot create target because no os was provided"
-    exit 1
   fi
 }
 
@@ -49,28 +35,18 @@ function strip_executable() {
   fi
 }
 
-# Arguments:
-#   $1 = os
-function target_dir() {
-  if [[ "$1" == "$LINUX_ARMV6" ]]; then
-    echo "target/arm-unknown-linux-gnueabihf/release"
-    return
-  fi
-  echo "target/release"
-}
-
-if [[ -z $1 || -z $2 ]]; then usage; fi
+if [[ -z $1 || -z $2 || -z $3 ]]; then usage; fi
 if ! is_supported_os "$2"; then usage; fi
 
 version="$1"
 os="$2"
+target="$3"
 
-target=$(target_by_os "$os")
 output="dra-$version-$target"
 extension=$([[ "$os" == "$WINDOWS" ]] && echo "zip" || echo "tar.gz")
 archive="${output}.${extension}"
 
-target_dir=$(target_dir "$os")
+target_dir="target/$target/release"
 
 mkdir -p "$output"
 
@@ -84,7 +60,7 @@ cp README.md "$output"
 cp LICENSE "$output"
 
 if [[ "$os" == "$WINDOWS" ]]; then
-  7z -y a "$archive" "$output" > /dev/null
+  7z -y a "$archive" "$output" >/dev/null
 else
   tar czf "$archive" "$output"
 fi
