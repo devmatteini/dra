b9e34db61648d9ba8ad26ade8ed558657f879a01
Cosimo Matteini <dev.matteini@gmail.com>
2023-02-04 17:19:09 +0100

    release: build and release linux musl

--- scripts/create-release-archive.sh
+++ scripts/create-release-archive.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 LINUX="linux"
+LINUX_MUSL="linux-musl"
 LINUX_ARMV6="linux-armv6"
 LINUX_ARM64="linux-arm64"
 MACOS="macos"
@@ -14,13 +15,13 @@ function usage() {
   echo
   echo "ARGS:"
   echo "    <version>     version using format x.y.z"
-  echo "    <os>          supported os: $LINUX, $MACOS, $WINDOWS, $LINUX_ARMV6, $LINUX_ARM64"
+  echo "    <os>          supported os: $LINUX, $MACOS, $WINDOWS, $LINUX_ARMV6, $LINUX_ARM64, $LINUX_MUSL"
   echo "    <target>      build target name (https://doc.rust-lang.org/rustc/platform-support.html)"
   exit 1
 }
 
 function is_supported_os() {
-  if [[ "$1" != "$LINUX" && "$1" != "$MACOS" && "$1" != "$WINDOWS" && "$1" != "$LINUX_ARMV6" && "$1" != "$LINUX_ARM64" ]]; then
+  if [[ "$1" != "$LINUX" && "$1" != "$MACOS" && "$1" != "$WINDOWS" && "$1" != "$LINUX_ARMV6" && "$1" != "$LINUX_ARM64" && "$1" != "$LINUX_MUSL" ]]; then
     echo "Error: '$1' is not valid os"
     return 1
   fi
@@ -29,7 +30,7 @@ function is_supported_os() {
 # Arguments:
 #   $1 = os; $2 = file to strip
 function strip_executable() {
-  if [[ "$1" == "$LINUX" || "$1" == "$MACOS" ]]; then
+  if [[ "$1" == "$LINUX" || "$1" == "$LINUX_MUSL" || "$1" == "$MACOS" ]]; then
     strip "$2"
   elif [[ "$1" == "$LINUX_ARMV6" ]]; then
     arm-linux-gnueabihf-strip "$2"
