40cf0d100f779a38205d75e6ea043afcfe36464b
Cosimo Matteini <dev.matteini@gmail.com>
2024-11-21 00:10:28 +0100

    installer: implement Display trait for ArchiveInstallerError

--- src/installer/error.rs
+++ src/installer/error.rs
@@ -46,11 +46,48 @@ impl std::fmt::Display for InstallError {
             InstallError::NotSupported(msg) => f.write_str(msg),
             InstallError::Fatal(msg) => f.write_str(msg),
             InstallError::NoExecutable => f.write_str("No executable found"),
-            InstallError::Archive(_error) => {
-                // TODO: create proper error message to show successes and failures
-                f.write_str("Archive error!!!")
+            InstallError::Archive(error) => {
+                let message = format!("{}", error);
+                f.write_str(&message)
             }
         }
+    }
+}
+
+impl std::fmt::Display for ArchiveInstallerError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let successes = self.successes.join("\n").to_string();
+        f.write_str(&successes)?;
+
+        let some_or_all = if self.successes.is_empty() {
+            "all"
+        } else {
+            "some"
+        };
+        let failures = format!("Failed to install {} executables:\n", some_or_all);
+        f.write_str(&failures)?;
+        for ArchiveError(executable, error) in self.failures.iter() {
+            match error {
+                ArchiveErrorType::ExecutableNotFound => {
+                    let message = format!("Executable {} not found", executable);
+                    f.write_str(&message)?;
+                }
+                ArchiveErrorType::TooManyExecutableCandidates(candidates) => {
+                    f.write_str("Many executable candidates found, you must select one:\n")?;
+                    for candidate in candidates {
+                        let message = format!("- {}\n", candidate);
+                        f.write_str(&message)?;
+                    }
+                    f.write_str("\nYou can use --install-file <INSTALL_FILE> instead")?;
+                }
+                ArchiveErrorType::Fatal(msg) => {
+                    let message = format!("{}: {}", executable, msg);
+                    f.write_str(&message)?;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
 
