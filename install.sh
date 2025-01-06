5f6567bcfae149a50d88e8591c4138a4a59d4fe7
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-06 23:44:12 +0100

    create install.sh script to automate the first installation of dra (#263)

---
+++ install.sh
@@ -0,0 +1,142 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GITHUB_REPOSITORY=devmatteini/dra
+
+info(){
+  echo -e "$1" >&2
+}
+
+error(){
+  echo -e "$1" >&2
+  exit 1
+}
+
+check_has_installed(){
+    if ! command -v "$1" &>/dev/null; then
+        echo "$1 is not installed"
+        return 1
+    fi
+}
+
+http_get(){
+  local url="$1"
+  local output_path="$2"
+
+  if command -v curl &>/dev/null; then
+    curl --proto =https --tlsv1.2 -sSfL -o "$output_path" "$url"
+  elif command -v wget &>/dev/null; then
+    wget --https-only --secure-protocol=TLSv1_2 --quiet -O "$output_path" "$url"
+  else
+    error "Missing curl and wget, you need to install one of the two"
+  fi
+}
+
+load_latest_release(){
+  local stdout="-"
+
+  http_get "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" "$stdout" |
+    grep tag_name |
+    cut -d'"' -f4
+}
+
+get_os(){
+  local os
+  os=$(uname -s)
+  case "$os" in
+    MINGW* | Win*) echo "Windows" ;;
+    *) echo "$os" ;;
+  esac
+}
+
+get_arch(){
+  uname -m
+}
+
+get_target(){
+  local os=$1
+  local arch=$2
+  local system="$arch-$os"
+
+  case "$system" in
+    arm64-Darwin) echo "aarch64-apple-darwin" ;;
+    aarch64-Linux) echo "aarch64-unknown-linux-gnu" ;;
+    armv6l-Linux) echo "arm-unknown-linux-gnueabihf" ;;
+    armv7l-Linux) echo "arm-unknown-linux-gnueabihf" ;;
+    x86_64-Darwin) echo "x86_64-apple-darwin" ;;
+    x86_64-Windows) echo "x86_64-pc-windows-msvc" ;;
+    x86_64-Linux) echo "x86_64-unknown-linux-musl" ;;
+    *) error "Unsupported system: $system" ;;
+  esac
+}
+
+get_archive_extension(){
+  local target=$1
+
+  case "$target" in
+    *windows*) echo "zip" ;;
+    *) echo "tar.gz" ;;
+  esac
+}
+
+download_asset(){
+  local version=$1
+  local asset=$2
+  local temp_dir=$3
+  local output_path="$temp_dir/$asset"
+
+  http_get "https://github.com/$GITHUB_REPOSITORY/releases/download/$version/$asset" "$output_path"
+  echo "$output_path"
+}
+
+extract_archive(){
+  local asset_path=$1
+  local output_dir=$2
+
+  case "$asset_path" in
+    *zip) unzip -j -d "$output_dir" "$asset_path" ;;
+    *tar.gz) tar xf "$asset_path" --strip-components=1 -C "$output_dir" ;;
+    *) error "Unknown archive $asset_path" ;;
+  esac
+}
+
+copy_executable(){
+  local asset_dir=$1
+  local destination=$2
+  local os=$3
+
+  if [[ "$os" == "Windows" ]]; then
+    cp "$asset_dir"/dra.exe "$destination"
+  else
+    cp "$asset_dir"/dra "$destination"
+  fi
+}
+
+main(){
+  os=$(get_os)
+  arch=$(get_arch)
+  target=$(get_target "$os" "$arch")
+  archive_extension=$(get_archive_extension "$target")
+
+  version=$(load_latest_release)
+  asset="dra-$version-$target.$archive_extension"
+
+  info "OS:           $arch-$os"
+  info "Repository:   $GITHUB_REPOSITORY"
+  info "Release:      $version"
+  info "Asset:        $asset"
+
+  info "\nDownloading $asset"
+  temp_dir=$(mktemp -d)
+  asset_path=$(download_asset "$version" "$asset" "$temp_dir")
+
+  info "Extracting archive $asset_path"
+  destination="$PWD"
+  extract_archive "$asset_path" "$temp_dir"
+  copy_executable "$temp_dir" "$destination" "$os"
+
+  info "Installation completed!"
+}
+
+main "$@"
