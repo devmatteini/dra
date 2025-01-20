e0c1ac8da24f602c1b13b8ed2c21b977cf758749
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-20 23:07:46 +0100

    github: add all possible env variables in error messages

--- src/github/error.rs
+++ src/github/error.rs
@@ -1,3 +1,4 @@
+use crate::github::constants::{DRA_GITHUB_TOKEN, GH_TOKEN, GITHUB_TOKEN};
 use std::fmt::Formatter;
 
 #[derive(Debug)]
@@ -21,6 +22,10 @@ impl GithubError {
     }
 }
 
+fn authentication_tokens() -> String {
+    format!("{} / {} / {}", DRA_GITHUB_TOKEN, GITHUB_TOKEN, GH_TOKEN)
+}
+
 impl std::fmt::Display for GithubError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -31,15 +36,23 @@ impl std::fmt::Display for GithubError {
             GithubError::RepositoryOrReleaseNotFound => {
                 f.write_str("Repository or release not found")
             }
-            GithubError::RateLimitExceeded => f.write_str(
-                "GitHub API rate limit exceeded.
-Export GITHUB_TOKEN environment variable to avoid this error.
+            GithubError::RateLimitExceeded => {
+                let message = format!(
+                    "GitHub API rate limit exceeded.
+Export one of {} environment variable to avoid this error.
 More information can be found at https://github.com/devmatteini/dra#usage",
-            ),
-            GithubError::Unauthorized => f.write_str(
-                "Invalid GitHub credentials.
-Make sure GITHUB_TOKEN is valid",
-            ),
+                    authentication_tokens()
+                );
+                f.write_str(&message)
+            }
+            GithubError::Unauthorized => {
+                let message = format!(
+                    "Invalid GitHub credentials.
+Make sure one of {} is valid",
+                    authentication_tokens()
+                );
+                f.write_str(&message)
+            }
         }
     }
 }
